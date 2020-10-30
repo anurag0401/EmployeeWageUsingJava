93790d45e8570dfc1fea59e7172a2823fcc3bd09
“anurag0401” <xoom.aj@gmail.com>
2020-10-30 16:11:41 +0530

    Wrote a program to calculate daily wage of an employee

--- wage.java
+++ wage.java
@@ -3,13 +3,23 @@ public class wage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
 
-        int IS_FULL_TIME = 1;
+        int Present = 1;
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_FULL_TIME)
+        if (empCheck == Present)
                 System.out.println("Present");
         else
                 System.out.println("Absent");
 
+	int empRatePerHr=20;
+	int empHrs=8;
+	int wage;
+	if ( empCheck == Present )
+	{
+		wage=empRatePerHr*empHrs;
+		System.out.println("Employee is present then DailyEmpWage:" +wage);
+	}
+	else
+		System.out.println("Employee is absent then DailyEmpWage: 0");
 }
 
 }
